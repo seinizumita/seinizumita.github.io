c0ed307fcda591787ed82b32aec828ad0e25dc5e
Sein Izumita <sein.izumita@gmail.com>
2021-05-30 22:23:01 -0400

    finished

--- LAB2/scripts/groceries.js
+++ LAB2/scripts/groceries.js
@@ -4,21 +4,73 @@
 
 var products = [
 	{
-		name: "brocoli",
-		vegetarian: true,
-		glutenFree: true,
+		name: "Brocoli",
+		lactoseFree: true,
+		nutFree: true,
+        organic: true,
 		price: 1.99
 	},
 	{
-		name: "bread",
-		vegetarian: true,
-		glutenFree: false,
+		name: "Bread",
+		lactoseFree: true,
+		nutFree: false,
+        organic: false,
 		price: 2.35
 	},
 	{
-		name: "salmon",
-		vegetarian: false,
-		glutenFree: true,
+		name: "2% Milk",
+		lactoseFree: false,
+		nutFree: true,
+        organic: false,
+		price: 10.00
+	},
+    {
+		name: "Oat Milk",
+		lactoseFree: true,
+		nutFree: true,
+        organic: true,
+		price: 10.00
+	},
+    {
+		name: "Assorted Nuts",
+		lactoseFree: true,
+		nutFree: false,
+        organic: true,
+		price: 10.00
+	},
+    {
+		name: "Peanut Butter",
+		lactoseFree: true,
+		nutFree: false,
+        organic: false,
+		price: 10.00
+	},
+    {
+		name: "Rice 2kg",
+		lactoseFree: true,
+		nutFree: true,
+        organic: true,
+		price: 10.00
+	},
+    {
+		name: "Bananas",
+		lactoseFree: true,
+		nutFree: true,
+        organic: true,
+		price: 10.00
+	},
+    {
+		name: "Strawberries",
+		lactoseFree: true,
+		nutFree: true,
+        organic: true,
+		price: 10.00
+	},
+    {
+		name: "Peanut Butter Ice Cream",
+		lactoseFree: false,
+		nutFree: true,
+        organic: false,
 		price: 10.00
 	}
 ];
@@ -31,10 +83,10 @@ var products = [
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+		if ((restriction == "LactoseFree") && (prods[i].lactoseFree == true)){
 			product_names.push(prods[i].name);
 		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+		else if ((restriction == "NutFree") && (prods[i].nutFree == true)){
 			product_names.push(prods[i].name);
 		}
 		else if (restriction == "None"){
@@ -42,6 +94,16 @@ function restrictListProducts(prods, restriction) {
 		}
 	}
 	return product_names;
+}
+
+//Fucntion to add the organic options in the products
+function organic(prods, restriction){
+    let product_names = [];
+    if (document.getElementById('dietSelect').checked)
+        if((restriction == "Organic") && (prods[i].organic == true)){
+            product_names.push(prods[i].name);
+    }
+    return product_names;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
@@ -52,5 +114,5 @@ function getTotalPrice(chosenProducts) {
 			totalPrice += products[i].price;
 		}
 	}
-	return totalPrice;
+	return "$" + totalPrice;
 }
